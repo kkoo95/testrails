4681dda871498b4cc0b06c8bd27efa23769fc8f7
K-kOo95 <kkoo95@gmail.com>
2013-08-18 03:43:13 +0200

    pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Testrails::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
+
   resources :microposts
 
   resources :users
@@ -7,7 +10,7 @@ Testrails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
